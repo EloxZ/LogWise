c08569a6f76ee2699eb211d2013697acf6ebeebe
EloxZ <45355879+EloxZ@users.noreply.github.com>
2024-02-29 00:09:03 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LogWise | Log your app in realtime</title>
+        <title>LogWise | Debug your remote app</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
